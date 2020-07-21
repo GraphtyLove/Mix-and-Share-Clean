c0ea1750596a04710f142b45c21b1eb0316fe2aa
maxim berge <maxim.berge@gmail.com>
2020-07-22 00:24:52 +0200

    add name to db container

--- scripts/db_run_rethinkdb_container.sh
+++ scripts/db_run_rethinkdb_container.sh
@@ -3,10 +3,10 @@ docker pull graphtylove/rethinkdb:latest;
 # Run the container and redirect container's ports:
 # 9090 (admin panel) to localhost port 3000
 # 28015 (client for request) to localhost port 3002
-docker run -p 3000:9090 -p 3002:28015 -it graphtylove/rethinkdb:latest;
+docker run --name "mix-and-share-db" -p 3000:9090 -p 3002:28015 -it graphtylove/rethinkdb:latest;
 # Create all the db and tables needed to make the project run
 echo "* ---------- RETHINK DB ---------- *"l;
 echo "DB Running...";
 echo "Client on port: 3002";
-echo "Admin UI on  port: 9090";
+echo "Admin UI on  port: 3002";
 echo "-------------------------------------";
